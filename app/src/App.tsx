aa91aed2c6db219acbb16fb4073ffb01ef0bdb7f
t_takumaru <t.takuma.saga.u@gmail.com>
2025-06-16 04:47:05 +0900

    websocketテスト用UI追加 #7

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -4,6 +4,8 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import GamePage from "./pages/GamePage/GamePage";
 import RoomListPage from "./pages/RoomListPage/RoomListPage";
 import StartPage from "./pages/StartPage/StartPage";
+import Lobby from "./components/Lobby/Lobby";
+import Game from "./components/Game/Game";
 
 function App() {
   return (
@@ -12,6 +14,8 @@ function App() {
         <Route path="/" element={<StartPage />} />
         <Route path="/rooms" element={<RoomListPage />} />
         <Route path="/game/:roomId" element={<GamePage />} />
+        <Route path="/lobbytest" element={<Lobby />}></Route>
+        <Route path="/gametest/:roomId" element={<Game />} />
       </Routes>
     </BrowserRouter>
   );
